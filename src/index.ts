bb1ba524e9d80f748b36f133a4e04442ced6fe02
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-09-22 12:30:24 -0300

    feature: add associations instance

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './routes'
 import dotenv from 'dotenv'
+import { associations } from './db/models/associatons'
 
 dotenv.config()
 
@@ -8,6 +9,8 @@ const app = express()
 const port = process.env.APP_PORT
 
 router(app)
+associations()
+
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
 export default app
